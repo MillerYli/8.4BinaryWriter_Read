9d34f8cf436816ad6aa968dab0976d7f7a328c5c
Yulia Miller <miller82.98@mail.ru>
2023-02-26 20:32:16 -0800

    Добавьте файлы проекта.

---
+++ 8.4BinaryWriter_Read/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+class BinaryExperiment
+{
+    const string SettingsFileName = @"c:\BinaryFile.bin";
+
+    static void Main()
+    {
+        // Считываем
+        ReadValues();
+    }
+
+    static void ReadValues()
+    {
+       
+        string StringValue;
+      
+
+        if (File.Exists(SettingsFileName))
+        {
+            // Создаем объект BinaryReader и инициализируем его возвратом метода File.Open.
+            using (BinaryReader reader = new BinaryReader(File.Open(SettingsFileName, FileMode.Open)))
+            {
+                // Применяем специализированные методы Read для считывания соответствующего типа данных.
+               
+                StringValue = reader.ReadString();
+                
+            }
+
+            Console.WriteLine("Из файла считано:");
+
+           
+            Console.WriteLine(StringValue);
+           
+        }
+    }
+}
